158db2b3aa0a3324a03e1b17a480afc7e09260a5
Calogero Ciaccio <calogerociaccio@MacBook-Pro-di-Calogero.local>
2024-09-09 03:06:53 +0200

    aggiornamento

--- src/components/About.js
+++ src/components/About.js
@@ -12,59 +12,59 @@ function About() {
     <div className="about">
       <div className="profile-header">
         <img src={process.env.PUBLIC_URL + '/profile-pic.jpg'} alt="Calogero Ciaccio" className="profile-pic" />
-        <h1 className="name-gradient" >Calogero Ciaccio</h1>
-        <p>Junior Front-End Developer</p>
+        <h1 className="name-gradient">Calogero Ciaccio</h1>
+        <p>Web Developer & Web Desing</p>
       </div>
 
       <div className="cv-card" onClick={toggleCV}>
-        <h2>Il Mio CV</h2>
-        <p>Clicca per {showCV ? 'nascondere' : 'visualizzare'} il CV</p>
+        <h2>My CV</h2>
+        <p>Click to {showCV ? 'hide' : 'view'} the CV</p>
       </div>
 
       {showCV && (
         <div className="cv-content">
-          <h2>Contatti</h2>
+          <h2>Contact Information</h2>
           <ul>
             <li>Email: caly92@live.it</li>
-            <li>Telefono: (+39) 3312997797</li>
-            <li>Indirizzo: Strada Cassia Nord, 61, 53100 Siena, Italia</li>
-            <li>LinkedIn: <a href="https://www.linkedin.com/in/calogero-ciaccio-528a361a1/" target="_blank" rel="noopener noreferrer">Profilo LinkedIn</a></li>
+            <li>Phone: (+39) 3312997797</li>
+            <li>Address: Strada Cassia Nord, 61, 53100 Siena, Italy</li>
+            <li>LinkedIn: <a href="https://www.linkedin.com/in/calogero-ciaccio-528a361a1/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></li>
           </ul>
 
-          <h2>Competenze Digitali</h2>
+          <h2>Digital Skills</h2>
           <ul>
-            <li>HTML, Javascript, JQuery, Ajax, Bootstrap, CSS</li>
-            <li>Java Script, TypeScript, jQuery, AngularJS, React e Node.js</li>
-            <li>Utilizzo sistemi Android e IOS</li>
-            <li>Ottima competenza nell'uso di sistemi operativi Apple e Windows</li>
+            <li>HTML, JavaScript, JQuery, Ajax, Bootstrap, CSS</li>
+            <li>JavaScript, TypeScript, jQuery, AngularJS, React, and Node.js</li>
+            <li>Experience with Android and iOS systems</li>
+            <li>Excellent proficiency with Apple and Windows operating systems</li>
           </ul>
 
-          <a href="/CV_CALOGERO_CIACCIO.pdf" download className="download-button">Scarica CV</a>
+          <a href="/CV_CALOGERO_CIACCIO.pdf" download className="download-button">Download CV</a>
         </div>
       )}
 
-      <h2>Chi Sono</h2>
+      <h2>About Me</h2>
       <p>
-        Ciao, sono Calogero Ciaccio, un Junior Front-End Developer appassionato di tecnologia e innovazione.
-        La mia passione per lo sviluppo web mi spinge costantemente a imparare nuove tecnologie e a migliorare le mie competenze.
+        Hi, I’m Calogero Ciaccio, a Junior Front-End Developer passionate about technology and innovation.
+        My passion for web development constantly drives me to learn new technologies and improve my skills.
       </p>
-      <h3>Le Mie Competenze are</h3>
+      <h3>My Skills</h3>
       <ul>
         <li>HTML5, CSS3, JavaScript</li>
         <li>React.js</li>
         <li>Responsive Web Design</li>
-        <li>Git e controllo versione</li>
+        <li>Git and version control</li>
         <li>UI/UX Design Basics</li>
       </ul>
-      <h3>Formazione</h3>
+      <h3>Education</h3>
       <p>
-        Ho completato un Bootcamp intensivo di Front-End Development presso Epicode School,
-        dove ho acquisito competenze pratiche e teoriche nel campo dello sviluppo web.
+        I completed an intensive Front-End Development Bootcamp at Epicode School,
+        where I gained practical and theoretical skills in web development.
       </p>
-      <h3>Obiettivi</h3>
+      <h3>Goals</h3>
       <p>
-        Il mio obiettivo è di crescere come sviluppatore, lavorando su progetti stimolanti
-        e contribuendo a creare esperienze web innovative e user-friendly.
+        My goal is to grow as a developer, working on stimulating projects
+        and contributing to creating innovative and user-friendly web experiences.
       </p>
     </div>
   );
