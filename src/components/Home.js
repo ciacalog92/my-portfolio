fb6c4d9f0efa1a42916f421494614cb902607b72
Calogero Ciaccio <calogerociaccio@MacBook-Pro-di-Calogero.local>
2024-09-11 14:29:41 +0200

    screen add

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,58 +29,30 @@ function Home() {
 
   return (
     <div className="Home">
-    <section className="white-section">
-      <div className="video-container">
-        <video 
-          src={videoSrc} 
-          autoPlay 
-          muted 
-          loop 
-          playsInline
-          className="responsive-video"
-        />
-      </div>
-    </section>
+      <section className="white-section">
+        <div className="video-container">
+          <video
+            src={videoSrc}
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="responsive-video"
+          />
+        </div>
+      </section>
 
       <section class="black-section">
         <div class="scroll-container">
           <div class="page">
-          <h3>STATIC WEBSITE</h3>
-          <p>
-            Creation of static and responsive websites with up to 5 pages, ideal
-            for presenting your business online.
-          </p>
-          <p className="tech-stack">Technologies: HTML, CSS, JavaScript</p>
-          <button className="discover-button">Discover</button>
-          </div>
-          <div class="page">
-            <h3>Dynamic Website</h3>
-            <p>
-              Creation of dynamic websites with CMS (WordPress, Joomla),
-              including advanced SEO and performance optimization.
-            </p>
-            <p className="tech-stack">
-              Technologies: HTML, CSS, JavaScript, PHP, MySQL
-            </p>
-            <button className="discover-button">Discover</button>
-          </div>
-          <div class="page">
-          <h3>E-Commerce Solutions</h3>
-          <p>
-            Design and development of customized e-commerce platforms, with CRM
-            integration and automation tools.
-          </p>
-          <p className="tech-stack">
-            Technologies: HTML, CSS, JavaScript, React, Node.js, MongoDB
-          </p>
-          <button className="discover-button">Discover</button>
           </div>
         </div>
       </section>
+      
       {/* Card section */}
       <div className="card-container" ref={cardContainerRef}>
         <div className="card">
-         
+          
         </div>
         <div className="card">
           <h3>Dynamic Website</h3>
@@ -93,9 +65,17 @@ function Home() {
           </p>
           <button className="discover-button">Discover</button>
         </div>
-        <div className="card">
-          
-        </div>
+        <div className="card"> <h3>Dynamic Website</h3>
+          <p>
+            Creation of dynamic websites with CMS (WordPress, Joomla), including
+            advanced SEO and performance optimization.
+          </p>
+          <p className="tech-stack">
+            Technologies: HTML, CSS, JavaScript, PHP, MySQL
+          </p>
+          <button className="discover-button">Discover</button>
+
+          </div>
       </div>
 
       {/* Navigation dots */}
@@ -114,24 +94,15 @@ function Home() {
       {/* Social icons */}
       <div className="social-icons">
         <a
-          href="https://wa.me/393312997797"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          href="https://wa.me/393312997797"target="_blank"rel="noopener noreferrer">
           <FaWhatsapp />
         </a>
         <a
-          href="https://www.instagram.com/ciacciocalogero/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          href="https://www.instagram.com/ciacciocalogero/"target="_blank"rel="noopener noreferrer">
           <FaInstagram />
         </a>
         <a
-          href="https://www.linkedin.com/in/calogero-ciaccio-528a361a1/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          href="https://www.linkedin.com/in/calogero-ciaccio-528a361a1/"target="_blank"rel="noopener noreferrer">
           <FaLinkedin />
         </a>
       </div>
